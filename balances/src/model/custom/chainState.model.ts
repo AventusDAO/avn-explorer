1b62fe055384bc6557eab28f7c63d731113a0985
IvanCholakov <ivan.cholakov@aventus.io>
2022-11-18 16:47:16 +0200

    feat[SYS-2886]: added precision and scale to numeric values in db schema

--- balances/src/model/custom/chainState.model.ts
+++ balances/src/model/custom/chainState.model.ts
@@ -1,28 +1,26 @@
-import { Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, Index as Index_ } from 'typeorm'
-import * as marshal from '../generated/marshal'
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, Index as Index_} from "typeorm"
+import * as marshal from "../generated/marshal"
 
 @Entity_()
-export class CurrentChainState {
-    constructor(props?: Partial<CurrentChainState>) {
-        Object.assign(this, props)
-    }
+export class ChainState {
+  constructor(props?: Partial<ChainState>) {
+    Object.assign(this, props)
+  }
 
-    @PrimaryColumn_()
-    id!: string
+  @PrimaryColumn_()
+  id!: string
 
-    @Column_('numeric', { transformer: marshal.bigintTransformer, nullable: false })
-    tokenBalance!: bigint
+  @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false, precision:80, scale: 0})
+  tokenBalance!: bigint
 
-    @Column_('int4', { nullable: false })
-    tokenHolders!: number
+  @Column_("int4", {nullable: false})
+  tokenHolders!: number
 
-    @Index_()
-    @Column_('timestamp with time zone', { nullable: false })
-    timestamp!: Date
+  @Index_()
+  @Column_("timestamp with time zone", {nullable: false})
+  timestamp!: Date
 
-    @Index_()
-    @Column_('int4', { nullable: false })
-    blockNumber!: number
-
-    // todo: add chain spec to differentiate chain origin if we're going for single db for solo+para chains combo
+  @Index_()
+  @Column_("int4", {nullable: false})
+  blockNumber!: number
 }
