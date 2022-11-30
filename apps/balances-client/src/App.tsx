00881fa679cd3d1eece6bc8e7d1f1507ee593306
IvanCholakov <ivan.cholakov@aventus.io>
2022-11-30 17:22:38 +0200

    feat[NO-JIRA]: initial setup of frontend

---
+++ apps/balances-client/src/App.tsx
@@ -0,0 +1,10 @@
+import './index.css'
+function App() {
+  return (
+    <div className='bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12'>
+      <h2 className='text-4xl text-yellow-500'>Hello world</h2>
+    </div>
+  )
+}
+
+export default App
