9404fd267153af4297f3c83edfe0bcf60d320191
Andrzej Frankowski <andrewfrankowski@gmail.com>
2022-12-12 10:57:50 +0100

    Update types. Skip in .prettierignore generated types

--- apps/balances/src/types/generated/parachain-dev/storage.ts
+++ apps/balances/src/types/generated/parachain-dev/storage.ts
@@ -3,191 +3,172 @@ import {Block, Chain, ChainContext, BlockContext, Result, Option} from './suppor
 import * as v4 from './v4'
 
 export class BalancesAccountStorage {
-  private readonly _chain: Chain
-  private readonly blockHash: string
+    private readonly _chain: Chain
+    private readonly blockHash: string
 
-  constructor(ctx: BlockContext)
-  constructor(ctx: ChainContext, block: Block)
-  constructor(ctx: BlockContext, block?: Block) {
-    block = block || ctx.block
-    this.blockHash = block.hash
-    this._chain = ctx._chain
-  }
+    constructor(ctx: BlockContext)
+    constructor(ctx: ChainContext, block: Block)
+    constructor(ctx: BlockContext, block?: Block) {
+        block = block || ctx.block
+        this.blockHash = block.hash
+        this._chain = ctx._chain
+    }
 
-  /**
-   *  The Balances pallet example of storing the balance of an account.
-   *
-   *  # Example
-   *
-   *  ```nocompile
-   *   impl pallet_balances::Config for Runtime {
-   *     type AccountStore = StorageMapShim<Self::Account<Runtime>, frame_system::Provider<Runtime>, AccountId, Self::AccountData<Balance>>
-   *   }
-   *  ```
-   *
-   *  You can also store the balance of an account in the `System` pallet.
-   *
-   *  # Example
-   *
-   *  ```nocompile
-   *   impl pallet_balances::Config for Runtime {
-   *    type AccountStore = System
-   *   }
-   *  ```
-   *
-   *  But this comes with tradeoffs, storing account balances in the system pallet stores
-   *  `frame_system` data alongside the account data contrary to storing account balances in the
-   *  `Balances` pallet, which uses a `StorageMap` to store balances data only.
-   *  NOTE: This is only used in the case that this pallet is used to store balances.
-   */
-  get isV4() {
-    return (
-      this._chain.getStorageItemTypeHash('Balances', 'Account') ===
-      '0b3b4bf0dd7388459eba461bc7c3226bf58608c941710a714e02f33ec0f91e78'
-    )
-  }
+    /**
+     *  The Balances pallet example of storing the balance of an account.
+     * 
+     *  # Example
+     * 
+     *  ```nocompile
+     *   impl pallet_balances::Config for Runtime {
+     *     type AccountStore = StorageMapShim<Self::Account<Runtime>, frame_system::Provider<Runtime>, AccountId, Self::AccountData<Balance>>
+     *   }
+     *  ```
+     * 
+     *  You can also store the balance of an account in the `System` pallet.
+     * 
+     *  # Example
+     * 
+     *  ```nocompile
+     *   impl pallet_balances::Config for Runtime {
+     *    type AccountStore = System
+     *   }
+     *  ```
+     * 
+     *  But this comes with tradeoffs, storing account balances in the system pallet stores
+     *  `frame_system` data alongside the account data contrary to storing account balances in the
+     *  `Balances` pallet, which uses a `StorageMap` to store balances data only.
+     *  NOTE: This is only used in the case that this pallet is used to store balances.
+     */
+    get isV4() {
+        return this._chain.getStorageItemTypeHash('Balances', 'Account') === '0b3b4bf0dd7388459eba461bc7c3226bf58608c941710a714e02f33ec0f91e78'
+    }
 
-  /**
-   *  The Balances pallet example of storing the balance of an account.
-   *
-   *  # Example
-   *
-   *  ```nocompile
-   *   impl pallet_balances::Config for Runtime {
-   *     type AccountStore = StorageMapShim<Self::Account<Runtime>, frame_system::Provider<Runtime>, AccountId, Self::AccountData<Balance>>
-   *   }
-   *  ```
-   *
-   *  You can also store the balance of an account in the `System` pallet.
-   *
-   *  # Example
-   *
-   *  ```nocompile
-   *   impl pallet_balances::Config for Runtime {
-   *    type AccountStore = System
-   *   }
-   *  ```
-   *
-   *  But this comes with tradeoffs, storing account balances in the system pallet stores
-   *  `frame_system` data alongside the account data contrary to storing account balances in the
-   *  `Balances` pallet, which uses a `StorageMap` to store balances data only.
-   *  NOTE: This is only used in the case that this pallet is used to store balances.
-   */
-  async getAsV4(key: Uint8Array): Promise<v4.AccountData> {
-    assert(this.isV4)
-    return this._chain.getStorage(this.blockHash, 'Balances', 'Account', key)
-  }
+    /**
+     *  The Balances pallet example of storing the balance of an account.
+     * 
+     *  # Example
+     * 
+     *  ```nocompile
+     *   impl pallet_balances::Config for Runtime {
+     *     type AccountStore = StorageMapShim<Self::Account<Runtime>, frame_system::Provider<Runtime>, AccountId, Self::AccountData<Balance>>
+     *   }
+     *  ```
+     * 
+     *  You can also store the balance of an account in the `System` pallet.
+     * 
+     *  # Example
+     * 
+     *  ```nocompile
+     *   impl pallet_balances::Config for Runtime {
+     *    type AccountStore = System
+     *   }
+     *  ```
+     * 
+     *  But this comes with tradeoffs, storing account balances in the system pallet stores
+     *  `frame_system` data alongside the account data contrary to storing account balances in the
+     *  `Balances` pallet, which uses a `StorageMap` to store balances data only.
+     *  NOTE: This is only used in the case that this pallet is used to store balances.
+     */
+    async getAsV4(key: Uint8Array): Promise<v4.AccountData> {
+        assert(this.isV4)
+        return this._chain.getStorage(this.blockHash, 'Balances', 'Account', key)
+    }
 
-  async getManyAsV4(keys: Uint8Array[]): Promise<v4.AccountData[]> {
-    assert(this.isV4)
-    return this._chain.queryStorage(
-      this.blockHash,
-      'Balances',
-      'Account',
-      keys.map(k => [k])
-    )
-  }
+    async getManyAsV4(keys: Uint8Array[]): Promise<(v4.AccountData)[]> {
+        assert(this.isV4)
+        return this._chain.queryStorage(this.blockHash, 'Balances', 'Account', keys.map(k => [k]))
+    }
 
-  async getAllAsV4(): Promise<v4.AccountData[]> {
-    assert(this.isV4)
-    return this._chain.queryStorage(this.blockHash, 'Balances', 'Account')
-  }
+    async getAllAsV4(): Promise<(v4.AccountData)[]> {
+        assert(this.isV4)
+        return this._chain.queryStorage(this.blockHash, 'Balances', 'Account')
+    }
 
-  /**
-   * Checks whether the storage item is defined for the current chain version.
-   */
-  get isExists(): boolean {
-    return this._chain.getStorageItemTypeHash('Balances', 'Account') != null
-  }
+    /**
+     * Checks whether the storage item is defined for the current chain version.
+     */
+    get isExists(): boolean {
+        return this._chain.getStorageItemTypeHash('Balances', 'Account') != null
+    }
 }
 
 export class BalancesTotalIssuanceStorage {
-  private readonly _chain: Chain
-  private readonly blockHash: string
+    private readonly _chain: Chain
+    private readonly blockHash: string
 
-  constructor(ctx: BlockContext)
-  constructor(ctx: ChainContext, block: Block)
-  constructor(ctx: BlockContext, block?: Block) {
-    block = block || ctx.block
-    this.blockHash = block.hash
-    this._chain = ctx._chain
-  }
+    constructor(ctx: BlockContext)
+    constructor(ctx: ChainContext, block: Block)
+    constructor(ctx: BlockContext, block?: Block) {
+        block = block || ctx.block
+        this.blockHash = block.hash
+        this._chain = ctx._chain
+    }
 
-  /**
-   *  The total units issued in the system.
-   */
-  get isV4() {
-    return (
-      this._chain.getStorageItemTypeHash('Balances', 'TotalIssuance') ===
-      'f8ebe28eb30158172c0ccf672f7747c46a244f892d08ef2ebcbaadde34a26bc0'
-    )
-  }
+    /**
+     *  The total units issued in the system.
+     */
+    get isV4() {
+        return this._chain.getStorageItemTypeHash('Balances', 'TotalIssuance') === 'f8ebe28eb30158172c0ccf672f7747c46a244f892d08ef2ebcbaadde34a26bc0'
+    }
 
-  /**
-   *  The total units issued in the system.
-   */
-  async getAsV4(): Promise<bigint> {
-    assert(this.isV4)
-    return this._chain.getStorage(this.blockHash, 'Balances', 'TotalIssuance')
-  }
+    /**
+     *  The total units issued in the system.
+     */
+    async getAsV4(): Promise<bigint> {
+        assert(this.isV4)
+        return this._chain.getStorage(this.blockHash, 'Balances', 'TotalIssuance')
+    }
 
-  /**
-   * Checks whether the storage item is defined for the current chain version.
-   */
-  get isExists(): boolean {
-    return this._chain.getStorageItemTypeHash('Balances', 'TotalIssuance') != null
-  }
+    /**
+     * Checks whether the storage item is defined for the current chain version.
+     */
+    get isExists(): boolean {
+        return this._chain.getStorageItemTypeHash('Balances', 'TotalIssuance') != null
+    }
 }
 
 export class SystemAccountStorage {
-  private readonly _chain: Chain
-  private readonly blockHash: string
+    private readonly _chain: Chain
+    private readonly blockHash: string
 
-  constructor(ctx: BlockContext)
-  constructor(ctx: ChainContext, block: Block)
-  constructor(ctx: BlockContext, block?: Block) {
-    block = block || ctx.block
-    this.blockHash = block.hash
-    this._chain = ctx._chain
-  }
+    constructor(ctx: BlockContext)
+    constructor(ctx: ChainContext, block: Block)
+    constructor(ctx: BlockContext, block?: Block) {
+        block = block || ctx.block
+        this.blockHash = block.hash
+        this._chain = ctx._chain
+    }
 
-  /**
-   *  The full account information for a particular account ID.
-   */
-  get isV4() {
-    return (
-      this._chain.getStorageItemTypeHash('System', 'Account') ===
-      '1ddc7ade926221442c388ee4405a71c9428e548fab037445aaf4b3a78f4735c1'
-    )
-  }
+    /**
+     *  The full account information for a particular account ID.
+     */
+    get isV4() {
+        return this._chain.getStorageItemTypeHash('System', 'Account') === '1ddc7ade926221442c388ee4405a71c9428e548fab037445aaf4b3a78f4735c1'
+    }
 
-  /**
-   *  The full account information for a particular account ID.
-   */
-  async getAsV4(key: Uint8Array): Promise<v4.AccountInfo> {
-    assert(this.isV4)
-    return this._chain.getStorage(this.blockHash, 'System', 'Account', key)
-  }
+    /**
+     *  The full account information for a particular account ID.
+     */
+    async getAsV4(key: Uint8Array): Promise<v4.AccountInfo> {
+        assert(this.isV4)
+        return this._chain.getStorage(this.blockHash, 'System', 'Account', key)
+    }
 
-  async getManyAsV4(keys: Uint8Array[]): Promise<v4.AccountInfo[]> {
-    assert(this.isV4)
-    return this._chain.queryStorage(
-      this.blockHash,
-      'System',
-      'Account',
-      keys.map(k => [k])
-    )
-  }
+    async getManyAsV4(keys: Uint8Array[]): Promise<(v4.AccountInfo)[]> {
+        assert(this.isV4)
+        return this._chain.queryStorage(this.blockHash, 'System', 'Account', keys.map(k => [k]))
+    }
 
-  async getAllAsV4(): Promise<v4.AccountInfo[]> {
-    assert(this.isV4)
-    return this._chain.queryStorage(this.blockHash, 'System', 'Account')
-  }
+    async getAllAsV4(): Promise<(v4.AccountInfo)[]> {
+        assert(this.isV4)
+        return this._chain.queryStorage(this.blockHash, 'System', 'Account')
+    }
 
-  /**
-   * Checks whether the storage item is defined for the current chain version.
-   */
-  get isExists(): boolean {
-    return this._chain.getStorageItemTypeHash('System', 'Account') != null
-  }
+    /**
+     * Checks whether the storage item is defined for the current chain version.
+     */
+    get isExists(): boolean {
+        return this._chain.getStorageItemTypeHash('System', 'Account') != null
+    }
 }
--- apps/tokens/src/types/generated/parachain-dev/storage.ts
+++ apps/tokens/src/types/generated/parachain-dev/storage.ts
@@ -1,55 +1,47 @@
 import assert from 'assert'
-import { Block, Chain, ChainContext, BlockContext, Result, Option } from './support'
+import {Block, Chain, ChainContext, BlockContext, Result, Option} from './support'
 
 export class TokenManagerBalancesStorage {
-  private readonly _chain: Chain
-  private readonly blockHash: string
+    private readonly _chain: Chain
+    private readonly blockHash: string
 
-  constructor(ctx: BlockContext)
-  constructor(ctx: ChainContext, block: Block)
-  constructor(ctx: BlockContext, block?: Block) {
-    block = block || ctx.block
-    this.blockHash = block.hash
-    this._chain = ctx._chain
-  }
+    constructor(ctx: BlockContext)
+    constructor(ctx: ChainContext, block: Block)
+    constructor(ctx: BlockContext, block?: Block) {
+        block = block || ctx.block
+        this.blockHash = block.hash
+        this._chain = ctx._chain
+    }
 
-  /**
-   *  The number of units of tokens held by any given account.
-   */
-  get isV4() {
-    return (
-      this._chain.getStorageItemTypeHash('TokenManager', 'Balances') ===
-      '70874cd5f158184b9b398819cb71b85684c5adb4e5cc9e1065d1115dac23d5f2'
-    )
-  }
+    /**
+     *  The number of units of tokens held by any given account.
+     */
+    get isV4() {
+        return this._chain.getStorageItemTypeHash('TokenManager', 'Balances') === '70874cd5f158184b9b398819cb71b85684c5adb4e5cc9e1065d1115dac23d5f2'
+    }
 
-  /**
-   *  The number of units of tokens held by any given account.
-   */
-  async getAsV4(key: [Uint8Array, Uint8Array]): Promise<bigint> {
-    assert(this.isV4)
-    return this._chain.getStorage(this.blockHash, 'TokenManager', 'Balances', key)
-  }
+    /**
+     *  The number of units of tokens held by any given account.
+     */
+    async getAsV4(key: [Uint8Array, Uint8Array]): Promise<bigint> {
+        assert(this.isV4)
+        return this._chain.getStorage(this.blockHash, 'TokenManager', 'Balances', key)
+    }
 
-  async getManyAsV4(keys: [Uint8Array, Uint8Array][]): Promise<bigint[]> {
-    assert(this.isV4)
-    return this._chain.queryStorage(
-      this.blockHash,
-      'TokenManager',
-      'Balances',
-      keys.map(k => [k])
-    )
-  }
+    async getManyAsV4(keys: [Uint8Array, Uint8Array][]): Promise<(bigint)[]> {
+        assert(this.isV4)
+        return this._chain.queryStorage(this.blockHash, 'TokenManager', 'Balances', keys.map(k => [k]))
+    }
 
-  async getAllAsV4(): Promise<bigint[]> {
-    assert(this.isV4)
-    return this._chain.queryStorage(this.blockHash, 'TokenManager', 'Balances')
-  }
+    async getAllAsV4(): Promise<(bigint)[]> {
+        assert(this.isV4)
+        return this._chain.queryStorage(this.blockHash, 'TokenManager', 'Balances')
+    }
 
-  /**
-   * Checks whether the storage item is defined for the current chain version.
-   */
-  get isExists(): boolean {
-    return this._chain.getStorageItemTypeHash('TokenManager', 'Balances') != null
-  }
+    /**
+     * Checks whether the storage item is defined for the current chain version.
+     */
+    get isExists(): boolean {
+        return this._chain.getStorageItemTypeHash('TokenManager', 'Balances') != null
+    }
 }
