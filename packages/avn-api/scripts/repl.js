22fa432e7e76c5b62a7eb43d3caac4a6dd578a88
Andrzej Frankowski <andrewfrankowski@gmail.com>
2022-12-13 18:31:45 +0100

    Fix eslint

--- packages/avn-api/scripts/repl.js
+++ packages/avn-api/scripts/repl.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 require('dotenv').config('../.env')
 const getApi = require('../lib/avnApi').getApi
 
