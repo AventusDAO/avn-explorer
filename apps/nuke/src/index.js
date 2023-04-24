7c12a288c3ed513f1cf8ba1ff29e5c80ab2a981f
Andrzej Frankowski <andrewfrankowski@gmail.com>
2023-04-24 17:07:10 +0200

    SYS-3291: Create nuke package with db schema reset script

---
+++ apps/nuke/src/index.js
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/no-var-requires */
+require('dotenv').config('../env')
+const { Client } = require('pg')
+
+const client = new Client({
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST ?? 'localhost'
+})
+
+const dropTables = async schema => {
+  console.log(`Dropping tables in ${schema} schema...`)
+
+  // const query = `
+  //   SELECT
+  //     'DROP TABLE IF EXISTS "' || schemaname || '.' || tablename || '" CASCADE;'
+  //   from
+  //     pg_tables WHERE schemaname = '${schema}';
+  // `
+  const query = `SELECT schemaname, tablename FROM pg_tables WHERE schemaname = '${schema}';`
+  const res = await client.query(query)
+  const { rows: tables } = res
+
+  for (const { schemaname, tablename } of tables) {
+    const dropQuery = `DROP TABLE IF EXISTS ${schemaname}.${tablename};`
+    await client.query(dropQuery)
+  }
+
+  console.log(`Done.`)
+}
+
+;(async () => {
+  await client.connect()
+  await dropTables('fees')
+  await client.end()
+})()
