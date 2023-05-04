042cfbab67651387b810888072510920742f3f62
IvanCholakov <ivan.cholakov@aventus.io>
2023-05-04 12:35:16 +0300

    SYS-3297 - added new processsor to monitor account transfers

---
+++ apps/account-monitor/src/model/generated/index.ts
@@ -0,0 +1,4 @@
+export * from "./account.model"
+export * from "./transfer.model"
+export * from "./_transferType"
+export * from "./chainState.model"
---
+++ apps/account-monitor/src/model/generated/transfer.model.ts
@@ -0,0 +1,49 @@
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, Index as Index_, ManyToOne as ManyToOne_} from "typeorm"
+import * as marshal from "./marshal"
+import {Account} from "./account.model"
+import {TransferType} from "./_transferType"
+
+@Entity_()
+export class Transfer {
+  constructor(props?: Partial<Transfer>) {
+    Object.assign(this, props)
+  }
+
+  @PrimaryColumn_()
+  id!: string
+
+  @Index_()
+  @Column_("int4", {nullable: false})
+  blockNumber!: number
+
+  @Index_()
+  @Column_("timestamp with time zone", {nullable: false})
+  timestamp!: Date
+
+  @Index_()
+  @Column_("text", {nullable: true})
+  extrinsicHash!: string | undefined | null
+
+  @Index_()
+  @Column_("text", {nullable: true})
+  call!: string | undefined | null
+
+  @Index_()
+  @ManyToOne_(() => Account, {nullable: true})
+  from!: Account
+
+  @Index_()
+  @ManyToOne_(() => Account, {nullable: true})
+  to!: Account
+
+  @Index_()
+  @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+  amount!: bigint
+
+  @Index_()
+  @Column_("text", {nullable: true})
+  tokenId!: string | undefined | null
+
+  @Column_("varchar", {length: 20, nullable: false})
+  transferType!: TransferType
+}
