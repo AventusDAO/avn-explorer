932a41863aa843cae4916b3d67245b5f74173da1
Andrzej Frankowski <andrewfrankowski@gmail.com>
2023-05-22 12:22:06 +0200

    SYS-3339: update processors metadata pt.2

--- apps/fees/src/handlers/feesHandler.ts
+++ apps/fees/src/handlers/feesHandler.ts
@@ -1,14 +1,14 @@
 import { UnknownVersionError } from '@avn/types'
 import { IEventHandler, IFeePaidData, TransactionPaymentFeesEventName } from '../types/custom'
-import { TransactionPaymentTransactionFeePaidEvent } from '../types/generated/parachain-dev/events'
+import { TransactionPaymentTransactionFeePaidEvent } from '../types/generated/parachain-testnet/events'
 
 export const handleFeePaid: IEventHandler = (ctx, event): IFeePaidData => {
   const data = new TransactionPaymentTransactionFeePaidEvent(ctx, event)
-  if (data.isV12) {
+  if (data.isV8) {
     return {
-      who: data.asV12.who,
-      actualFee: data.asV12.actualFee,
-      tip: data.asV12.tip
+      who: data.asV8.who,
+      actualFee: data.asV8.actualFee,
+      tip: data.asV8.tip
     }
   } else {
     throw new UnknownVersionError(event.name)
