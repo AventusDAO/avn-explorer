c68ed51e7dc92313cacb792534746624b074a758
Andrzej Frankowski <andrewfrankowski@gmail.com>
2023-05-24 10:31:43 +0200

    fix prettier

--- apps/fees/src/types/generated/parachain-testnet/events.ts
+++ apps/fees/src/types/generated/parachain-testnet/events.ts
@@ -1,33 +1,36 @@
 import assert from 'assert'
-import {Chain, ChainContext, EventContext, Event, Result, Option} from './support'
+import { Chain, ChainContext, EventContext, Event, Result, Option } from './support'
 
 export class TransactionPaymentTransactionFeePaidEvent {
-    private readonly _chain: Chain
-    private readonly event: Event
+  private readonly _chain: Chain
+  private readonly event: Event
 
-    constructor(ctx: EventContext)
-    constructor(ctx: ChainContext, event: Event)
-    constructor(ctx: EventContext, event?: Event) {
-        event = event || ctx.event
-        assert(event.name === 'TransactionPayment.TransactionFeePaid')
-        this._chain = ctx._chain
-        this.event = event
-    }
+  constructor(ctx: EventContext)
+  constructor(ctx: ChainContext, event: Event)
+  constructor(ctx: EventContext, event?: Event) {
+    event = event || ctx.event
+    assert(event.name === 'TransactionPayment.TransactionFeePaid')
+    this._chain = ctx._chain
+    this.event = event
+  }
 
-    /**
-     * A transaction fee `actual_fee`, of which `tip` was added to the minimum inclusion fee,
-     * has been paid by `who`.
-     */
-    get isV8(): boolean {
-        return this._chain.getEventHash('TransactionPayment.TransactionFeePaid') === 'f2e962e9996631445edecd62b0646df79871442a2d1a1a6e1f550a0b3a56b226'
-    }
+  /**
+   * A transaction fee `actual_fee`, of which `tip` was added to the minimum inclusion fee,
+   * has been paid by `who`.
+   */
+  get isV8(): boolean {
+    return (
+      this._chain.getEventHash('TransactionPayment.TransactionFeePaid') ===
+      'f2e962e9996631445edecd62b0646df79871442a2d1a1a6e1f550a0b3a56b226'
+    )
+  }
 
-    /**
-     * A transaction fee `actual_fee`, of which `tip` was added to the minimum inclusion fee,
-     * has been paid by `who`.
-     */
-    get asV8(): {who: Uint8Array, actualFee: bigint, tip: bigint} {
-        assert(this.isV8)
-        return this._chain.decodeEvent(this.event)
-    }
+  /**
+   * A transaction fee `actual_fee`, of which `tip` was added to the minimum inclusion fee,
+   * has been paid by `who`.
+   */
+  get asV8(): { who: Uint8Array; actualFee: bigint; tip: bigint } {
+    assert(this.isV8)
+    return this._chain.decodeEvent(this.event)
+  }
 }
