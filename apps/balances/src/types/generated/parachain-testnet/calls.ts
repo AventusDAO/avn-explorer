30fc7d4f5c27cb4e2db415f80862b9051d61797a
Andrzej Frankowski <andrewfrankowski@gmail.com>
2023-06-06 16:00:06 +0200

    SYS-2613: fix balances processor

---
+++ apps/balances/src/types/generated/parachain-testnet/calls.ts
@@ -0,0 +1,43 @@
+import assert from 'assert'
+import {Chain, ChainContext, CallContext, Call, Result, Option} from './support'
+
+export class MigrationMigrateSystemAccountCall {
+    private readonly _chain: Chain
+    private readonly call: Call
+
+    constructor(ctx: CallContext)
+    constructor(ctx: ChainContext, call: Call)
+    constructor(ctx: CallContext, call?: Call) {
+        call = call || ctx.call
+        assert(call.name === 'Migration.migrate_system_account')
+        this._chain = ctx._chain
+        this.call = call
+    }
+
+    /**
+     * Migrating the Account informations from frame_system.
+     * 
+     * This call takes the raw scale encoded key (= patricia-key for each account in the `Account` storage and inserts
+     * the provided scale encoded value (= `AccountInfo`) into the underlying DB.
+     * 
+     * Note: As we are converting from substrate-v2 to substrate-v3 we must do type-conversions. Those conversions are done
+     * off-chain.
+     */
+    get isV4(): boolean {
+        return this._chain.getCallHash('Migration.migrate_system_account') === '8f752997770c549f5b2110f1fefa80bf1ce3e4cfb892e26a4d0ccb769acf1202'
+    }
+
+    /**
+     * Migrating the Account informations from frame_system.
+     * 
+     * This call takes the raw scale encoded key (= patricia-key for each account in the `Account` storage and inserts
+     * the provided scale encoded value (= `AccountInfo`) into the underlying DB.
+     * 
+     * Note: As we are converting from substrate-v2 to substrate-v3 we must do type-conversions. Those conversions are done
+     * off-chain.
+     */
+    get asV4(): {accounts: [Uint8Array, Uint8Array][]} {
+        assert(this.isV4)
+        return this._chain.decodeCall(this.call)
+    }
+}
