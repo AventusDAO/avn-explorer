a61865f55d9f21f97ddd042154d851d83e9787dd
Andrzej Frankowski <andrewfrankowski@gmail.com>
2023-07-06 13:06:24 +0200

    Prettify errors

--- apps/errors/src/model/generated/chainState.model.ts
+++ apps/errors/src/model/generated/chainState.model.ts
@@ -1,19 +1,24 @@
-import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, Index as Index_} from "typeorm"
+import {
+  Entity as Entity_,
+  Column as Column_,
+  PrimaryColumn as PrimaryColumn_,
+  Index as Index_
+} from 'typeorm'
 
 @Entity_()
 export class ChainState {
-    constructor(props?: Partial<ChainState>) {
-        Object.assign(this, props)
-    }
+  constructor(props?: Partial<ChainState>) {
+    Object.assign(this, props)
+  }
 
-    @PrimaryColumn_()
-    id!: string
+  @PrimaryColumn_()
+  id!: string
 
-    @Index_()
-    @Column_("timestamp with time zone", {nullable: false})
-    timestamp!: Date
+  @Index_()
+  @Column_('timestamp with time zone', { nullable: false })
+  timestamp!: Date
 
-    @Index_()
-    @Column_("int4", {nullable: false})
-    blockNumber!: number
+  @Index_()
+  @Column_('int4', { nullable: false })
+  blockNumber!: number
 }
