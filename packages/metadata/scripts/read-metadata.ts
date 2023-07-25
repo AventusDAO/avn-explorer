1ea448e159aa564b2be59319c7da6662a8c300f2
Andrzej Frankowski <andrewfrankowski@gmail.com>
2023-07-25 13:24:35 +0200

    SYS-3154: add script to parse metadata for all event names
    cleanup
    
    prettier

---
+++ packages/metadata/scripts/read-metadata.ts
@@ -0,0 +1,39 @@
+import { Si1TypeDefVariant } from '@subsquid/substrate-metadata'
+import { getMetadata } from '../src/index'
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export const getAllEvents = (specId: string, versionsFileName: string) => {
+  const meta = getMetadata(versionsFileName, specId)
+  if (meta.__kind !== 'V14') throw new Error('Unsupported Metadata version')
+
+  const { pallets } = meta.value
+
+  const data = pallets.map(p => {
+    const eventsIndex = p.events?.type
+    if (eventsIndex === undefined) {
+      console.debug(`Cannot find event index for pallet ${p.name}`)
+      return {
+        name: p.name,
+        events: []
+      }
+    }
+    const eventType = meta.value.lookup.types.find(t => t.id === eventsIndex)
+    if (!eventType) {
+      console.debug(`Lookup types missing for event index ${eventsIndex} for pallet ${p.name}`)
+      return {
+        name: p.name,
+        events: []
+      }
+    }
+    const eventNames = (eventType.type.def.value as Si1TypeDefVariant).variants.map(v => v.name)
+    return {
+      name: p.name,
+      events: eventNames
+    }
+  })
+
+  return data
+}
+
+const eventsMetadata = getAllEvents('avn-parachain@31', 'versions.avn-parachain.dev.jsonl')
+console.log(eventsMetadata)
