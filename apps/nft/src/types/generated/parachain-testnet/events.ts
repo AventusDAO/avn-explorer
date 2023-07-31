eaaee8c5de322c9a7cd4e8be44945e7865b63044
Andrzej Frankowski <andrewfrankowski@gmail.com>
2023-07-31 15:24:17 +0200

    SYS-3569: update all typegen for required types

--- apps/nft/src/types/generated/parachain-testnet/events.ts
+++ apps/nft/src/types/generated/parachain-testnet/events.ts
@@ -1,5 +1,35 @@
 import assert from 'assert'
 import {Chain, ChainContext, EventContext, Event, Result, Option} from './support'
+import * as v4 from './v4'
+
+export class NftManagerBatchCreatedEvent {
+    private readonly _chain: Chain
+    private readonly event: Event
+
+    constructor(ctx: EventContext)
+    constructor(ctx: ChainContext, event: Event)
+    constructor(ctx: EventContext, event?: Event) {
+        event = event || ctx.event
+        assert(event.name === 'NftManager.BatchCreated')
+        this._chain = ctx._chain
+        this.event = event
+    }
+
+    /**
+     * batch_id, total_supply, batch_creator, provenance
+     */
+    get isV4(): boolean {
+        return this._chain.getEventHash('NftManager.BatchCreated') === '444e53a57d5de1115824210f79cf5345f22358b98591752e1535bd55f64323b0'
+    }
+
+    /**
+     * batch_id, total_supply, batch_creator, provenance
+     */
+    get asV4(): {batchNftId: bigint, totalSupply: bigint, batchCreator: Uint8Array, authority: Uint8Array} {
+        assert(this.isV4)
+        return this._chain.decodeEvent(this.event)
+    }
+}
 
 export class NftManagerBatchNftMintedEvent {
     private readonly _chain: Chain
@@ -25,6 +55,64 @@ export class NftManagerBatchNftMintedEvent {
      * nft_id, batch_id, provenance, owner
      */
     get asV4(): {nftId: bigint, batchNftId: bigint, authority: Uint8Array, owner: Uint8Array} {
+        assert(this.isV4)
+        return this._chain.decodeEvent(this.event)
+    }
+}
+
+export class NftManagerEthNftTransferEvent {
+    private readonly _chain: Chain
+    private readonly event: Event
+
+    constructor(ctx: EventContext)
+    constructor(ctx: ChainContext, event: Event)
+    constructor(ctx: EventContext, event?: Event) {
+        event = event || ctx.event
+        assert(event.name === 'NftManager.EthNftTransfer')
+        this._chain = ctx._chain
+        this.event = event
+    }
+
+    /**
+     * EthNftTransfer(NftId, NewOwnerAccountId, NftSaleType, u64, EthEventId),
+     */
+    get isV4(): boolean {
+        return this._chain.getEventHash('NftManager.EthNftTransfer') === 'dacd5568e1e613bf13bd61ec2a3dde12c269262c4e693508a6ba11a85c9626b5'
+    }
+
+    /**
+     * EthNftTransfer(NftId, NewOwnerAccountId, NftSaleType, u64, EthEventId),
+     */
+    get asV4(): {nftId: bigint, newOwner: Uint8Array, saleType: v4.NftSaleType, opId: bigint, ethEventId: v4.EthEventId} {
+        assert(this.isV4)
+        return this._chain.decodeEvent(this.event)
+    }
+}
+
+export class NftManagerFiatNftTransferEvent {
+    private readonly _chain: Chain
+    private readonly event: Event
+
+    constructor(ctx: EventContext)
+    constructor(ctx: ChainContext, event: Event)
+    constructor(ctx: EventContext, event?: Event) {
+        event = event || ctx.event
+        assert(event.name === 'NftManager.FiatNftTransfer')
+        this._chain = ctx._chain
+        this.event = event
+    }
+
+    /**
+     * FiatNftTransfer(NftId, SenderAccountId, NewOwnerAccountId, NftSaleType, NftNonce)
+     */
+    get isV4(): boolean {
+        return this._chain.getEventHash('NftManager.FiatNftTransfer') === 'bcc234ffdff6ace39dad4bcb0d4beab866926c346d9b4225c47099b247afde9b'
+    }
+
+    /**
+     * FiatNftTransfer(NftId, SenderAccountId, NewOwnerAccountId, NftSaleType, NftNonce)
+     */
+    get asV4(): {nftId: bigint, sender: Uint8Array, newOwner: Uint8Array, saleType: v4.NftSaleType, opId: bigint} {
         assert(this.isV4)
         return this._chain.decodeEvent(this.event)
     }
