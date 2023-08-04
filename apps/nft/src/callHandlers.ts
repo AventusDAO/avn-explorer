f1efdabaa7de6786fa0c14d9a73fc901a99e3170
Andrzej Frankowski <andrewfrankowski@gmail.com>
2023-08-04 13:13:17 +0200

    SYS-3569: move generic Proxy types to types package. Use in call handler
    prettier

--- apps/nft/src/callHandlers.ts
+++ apps/nft/src/callHandlers.ts
@@ -1,14 +1,16 @@
 import { Ctx } from '.'
-import { NftMintCallArgs, NftMintEventCall } from './types'
+import { NftMintCallArgs, NftMintEventCall, ProxyCallArgs } from './types'
 
 export function normalizeCallArgs(callItem: NftMintEventCall, _ctx: Ctx): NftMintCallArgs {
   const { args } = callItem
   if (!callItem.name.includes('proxy')) {
+    // NOTE: args.call.__kind === 'NftManager' && args.call.value.__kind === 'signed_mint_single_nft'
     return args.call.value
   }
 
-  // NOTE: args.call.__kind === 'NftManager' && args.call.value.__kind === 'signed_mint_single_nft'
-  const { royalties, t1Authority, uniqueExternalRef } = args.call.value
+  // NOTE: proxy call requires manual typing
+  const proxyArgs = args as ProxyCallArgs<NftMintCallArgs>
+  const { royalties, t1Authority, uniqueExternalRef } = proxyArgs.call.value
   return {
     royalties,
     t1Authority,
--- apps/nft/src/types/custom/call.ts
+++ apps/nft/src/types/custom/call.ts
@@ -1,6 +1,7 @@
 import { CallItem } from '@subsquid/substrate-processor/lib/interfaces/dataSelection'
 import { NftRoyalty } from '../../model'
 import { NftMintEventItem } from './event'
+export { ProxyCallArgs } from '@avn/types'
 
 export type MintCallItem = CallItem<'NftManager.SingleNftMinted', { call: { args: true } }>
 export type NftMintEventCall = Required<NftMintEventItem['event']>['call']
@@ -9,26 +10,4 @@ export interface NftMintCallArgs {
   royalties: NftRoyalty[]
   t1Authority: string
   uniqueExternalRef: string
-}
-
-export interface ProxyCallArgs<A> {
-  call: {
-    value: A & ProxyCallProof
-  }
-  paymentInfo: {
-    payer: string
-    amount: string
-    recipient: string
-    signature: {
-      value: string
-    }
-  }
-}
-
-export interface ProxyCallProof {
-  signer: string
-  relayer: string
-  signature: {
-    value: string
-  }
 }
