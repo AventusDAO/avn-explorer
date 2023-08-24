473abfc14ff0c962305066b05d3c46af8bed4753
Andrzej Frankowski <andrewfrankowski@gmail.com>
2023-08-24 17:44:13 +0200

    SYS-3583: further cleanup of generated types that are no longer needed

--- apps/nft/src/types/generated/parachain-dev/calls.ts
+++ apps/nft/src/types/generated/parachain-dev/calls.ts
@@ -1,35 +1,6 @@
 import assert from 'assert'
 import {Chain, ChainContext, CallContext, Call, Result, Option} from './support'
 
-export class MigrationMigrateBatchInfoIdsCall {
-    private readonly _chain: Chain
-    private readonly call: Call
-
-    constructor(ctx: CallContext)
-    constructor(ctx: ChainContext, call: Call)
-    constructor(ctx: CallContext, call?: Call) {
-        call = call || ctx.call
-        assert(call.name === 'Migration.migrate_batch_info_ids')
-        this._chain = ctx._chain
-        this.call = call
-    }
-
-    /**
-     * Migrates NftManager Nft Batch InfoIds from the AvN source chain
-     */
-    get isV21(): boolean {
-        return this._chain.getCallHash('Migration.migrate_batch_info_ids') === 'bb001fdf20a695f3672c2f97617f4530637ebbbe276c877a41305bb8848b77e2'
-    }
-
-    /**
-     * Migrates NftManager Nft Batch InfoIds from the AvN source chain
-     */
-    get asV21(): {infoIds: [Uint8Array, bigint][]} {
-        assert(this.isV21)
-        return this._chain.decodeCall(this.call)
-    }
-}
-
 export class MigrationMigrateNftBatchesCall {
     private readonly _chain: Chain
     private readonly call: Call
@@ -54,35 +25,6 @@ export class MigrationMigrateNftBatchesCall {
      * Migrates NftManager Nft Batches from the AvN source chain
      */
     get asV21(): {batches: [Uint8Array, Uint8Array][]} {
-        assert(this.isV21)
-        return this._chain.decodeCall(this.call)
-    }
-}
-
-export class MigrationMigrateNftInfosCall {
-    private readonly _chain: Chain
-    private readonly call: Call
-
-    constructor(ctx: CallContext)
-    constructor(ctx: ChainContext, call: Call)
-    constructor(ctx: CallContext, call?: Call) {
-        call = call || ctx.call
-        assert(call.name === 'Migration.migrate_nft_infos')
-        this._chain = ctx._chain
-        this.call = call
-    }
-
-    /**
-     * Migrates NftManager NftInfos from the AvN source chain
-     */
-    get isV21(): boolean {
-        return this._chain.getCallHash('Migration.migrate_nft_infos') === '7b646c0e0cd1b17309d284a54286a87248f44b798176a4edc399e35f439ec747'
-    }
-
-    /**
-     * Migrates NftManager NftInfos from the AvN source chain
-     */
-    get asV21(): {infos: [Uint8Array, Uint8Array][]} {
         assert(this.isV21)
         return this._chain.decodeCall(this.call)
     }
