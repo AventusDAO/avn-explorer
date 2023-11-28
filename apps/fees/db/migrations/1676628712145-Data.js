dd9d72096a9929701c8fbfe383924fde9560a49c
IvanCholakov <ivan.cholakov@aventus.io>
2023-11-28 16:29:57 +0200

    added permissions for explorer_ro

--- apps/fees/db/migrations/1676628712145-Data.js
+++ apps/fees/db/migrations/1676628712145-Data.js
@@ -8,6 +8,10 @@ module.exports = class Data1676628712145 {
         await db.query(`GRANT USAGE ON SCHEMA public To readonly`)
         await db.query(`GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly`)
         await db.query(`GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO readonly`)
+
+        await db.query(`GRANT USAGE ON SCHEMA public To explorer_ro`)
+        await db.query(`GRANT SELECT ON ALL TABLES IN SCHEMA public TO explorer_ro`)
+        await db.query(`GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO explorer_ro`)
     }
 
     async down(db) {
