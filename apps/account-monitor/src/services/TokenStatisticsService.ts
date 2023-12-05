ab327e1822b38769ae077660742c64b7db494bc4
IvanCholakov <ivan.cholakov@aventus.io>
2023-12-05 17:26:31 +0200

    fix: code review refactor

--- apps/account-monitor/src/services/TokenStatisticsService.ts
+++ apps/account-monitor/src/services/TokenStatisticsService.ts
@@ -1,10 +1,13 @@
-import { EntityManager } from 'typeorm'
+
+
+
+import { EntityManager } from 'typeorm';
 
 export class TokenTransferService {
-  constructor(private readonly manager: EntityManager) {}
+  constructor(private readonly manager: EntityManager) { }
 
   private formatDateForQuery(date: Date): string {
-    return date.toISOString()
+    return date.toISOString();
   }
 
   private async executeAggregateQuery(
@@ -16,8 +19,8 @@ export class TokenTransferService {
       const result = await this.manager.query(query, params)
       return BigInt(result?.[0]?.[output] ?? 0)
     } catch (error) {
-      console.error('Error executing aggregate query:', error)
-      throw new Error('Error executing database operation')
+      console.error('Error executing aggregate query:', error);
+      throw new Error('Error executing database operation');
     }
   }
 
