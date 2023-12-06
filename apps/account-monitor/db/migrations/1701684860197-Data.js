6e375f1f8f2087f9bef0b70594cccb9b497ecf5a
IvanCholakov <ivan.cholakov@aventus.io>
2023-12-06 18:42:32 +0200

    readded permissions

--- apps/account-monitor/db/migrations/1701684860197-Data.js
+++ apps/account-monitor/db/migrations/1701684860197-Data.js
@@ -7,9 +7,9 @@ module.exports = class Data1701684860197 {
         await db.query(`ALTER TABLE "token_transfer" ADD CONSTRAINT "FK_f969fe3a497c6ee19a182115442" FOREIGN KEY ("payer_id") REFERENCES "account"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`)
 
 
-        // await db.query(`GRANT USAGE ON SCHEMA public To readonly`)
-        // await db.query(`GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly`)
-        // await db.query(`GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO readonly`)
+        await db.query(`GRANT USAGE ON SCHEMA public To readonly`)
+        await db.query(`GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly`)
+        await db.query(`GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO readonly`)
     }
 
     async down(db) {
