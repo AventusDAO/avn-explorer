f21450662f9ff48165760350b59eabe9e4980d0a
IvanCholakov <ivan.cholakov@aventus.io>
2023-12-06 17:08:17 +0200

    code review suggestions applied

--- apps/account-monitor/src/services/TokenStatisticsService.ts
+++ apps/account-monitor/src/services/TokenStatisticsService.ts
@@ -21,14 +21,14 @@ export class TokenTransferService {
     }
   }
 
-  private buildSumQuery(method: string, isNegative: boolean = false): string {
-    return `SUM(CASE WHEN method = '${method}' THEN amount ELSE 0 END) ${isNegative ? '-' : ''} `
+  private buildSumQuery(method: string): string {
+    return `SUM(CASE WHEN method = '${method}' THEN amount ELSE 0 END)`
   }
 
   private buildCommonQuery(tokenId: string, accountId?: string): string {
     let query = `
       SELECT 
-        ${this.buildSumQuery('TokenLifted', true)}
+        ${this.buildSumQuery('TokenLifted')} -
         ${this.buildSumQuery('TokenLowered')}
         AS totalAmount
       FROM token_transfer
@@ -45,23 +45,27 @@ export class TokenTransferService {
 
   async getTotalAmountByToken(tokenId: string): Promise<bigint> {
     const query = this.buildCommonQuery(tokenId)
-    return await this.executeAggregateQuery(query, [tokenId], 'totalamount')
+    return await this.executeAggregateQuery(query, [tokenId], 'totalAmount')
   }
 
   async getTotalAmountByAccountAndToken(accountId: string, tokenId: string): Promise<bigint> {
     const query = this.buildCommonQuery(tokenId, accountId)
-    return await this.executeAggregateQuery(query, [accountId, tokenId], 'totalamount')
+    return await this.executeAggregateQuery(query, [accountId, tokenId], 'totalAmount')
   }
 
   async getAverageAmountLastNDays(n: number, tokenId: string, accountId?: string): Promise<bigint> {
     const dateAgo = this.formatDateForQuery(new Date(Date.now() - n * 24 * 60 * 60 * 1000))
+
     let query = `
-      SELECT AVG(${this.buildSumQuery('TokenLifted', true)}
-        ${this.buildSumQuery('TokenLowered')}
-) as averageAmount
-      FROM token_transfer
-      WHERE token_id = $1
-        AND timestamp >= $2
+    SELECT AVG(totalAmount) as averageAmount
+    FROM (
+        SELECT
+          ${this.buildSumQuery('TokenLifted')} -
+          ${this.buildSumQuery('TokenLowered')} as totalAmount
+        FROM token_transfer
+        WHERE token_id = $1
+          AND timestamp >= $2
+    ) as subquery
     `
 
     const params = [tokenId, dateAgo]
@@ -70,47 +74,47 @@ export class TokenTransferService {
       params.push(accountId)
     }
 
-    return await this.executeAggregateQuery(query, params, 'averageamount')
+    return await this.executeAggregateQuery(query, params, 'averageAmount')
   }
 
   async getTotalLoweredAmountByToken(tokenId: string): Promise<bigint> {
     const query = `
       SELECT SUM(amount) as totalAmount
       FROM token_transfer
-      WHERE token_id = $1 AND method = 'TokenLowered'
+      WHERE token_id = $1 AND method = 'TokenLowered' AND token_id != '0x0d88ed6e74bbfd96b831231638b66c05571e824f'
     `
 
-    return await this.executeAggregateQuery(query, [tokenId], 'totalamount')
+    return await this.executeAggregateQuery(query, [tokenId], 'totalAmount')
   }
 
   async getTotalLiftedAmountByToken(tokenId: string): Promise<bigint> {
     const query = `
       SELECT SUM(amount) as totalAmount
       FROM token_transfer
-      WHERE token_id = $1 AND method = 'TokenLifted'
+      WHERE token_id = $1 AND method = 'TokenLifted' AND token_id != '0x0d88ed6e74bbfd96b831231638b66c05571e824f'
     `
 
-    return await this.executeAggregateQuery(query, [tokenId], 'totalamount')
+    return await this.executeAggregateQuery(query, [tokenId], 'totalAmount')
   }
 
   async getTotalAvtLoweredAmount(): Promise<bigint> {
     const query = `
       SELECT SUM(amount) as totalAmount
       FROM token_transfer
-      WHERE method = 'AvtLowered'
+      WHERE method = 'AvtLowered' OR (method = 'TokenLowered' AND token_id = '0x0d88ed6e74bbfd96b831231638b66c05571e824f')
     `
 
-    return await this.executeAggregateQuery(query, [], 'totalamount')
+    return await this.executeAggregateQuery(query, [], 'totalAmount')
   }
 
   async getTotalAvtLiftedAmount(): Promise<bigint> {
     const query = `
       SELECT SUM(amount) as totalAmount
       FROM token_transfer
-      WHERE method = 'AVTLifted'
+      WHERE method = 'AVTLifted' OR (method = 'TokenLifted' AND token_id = '0x0d88ed6e74bbfd96b831231638b66c05571e824f')
     `
 
-    return await this.executeAggregateQuery(query, [], 'totalamount')
+    return await this.executeAggregateQuery(query, [], 'totalAmount')
   }
 
   async countTokenTransactionsByMethodForPeriod(
