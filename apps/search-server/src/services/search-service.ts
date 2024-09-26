665d52731fb4e8bcf20d1a0d2d060f0756acc534
Ivan Cholakov <ivan.cholakov@aventus.io>
2024-09-26 19:42:29 +0300

    SYS-3395: fix serch-server now can look for public keys (#181)
    * fix: serch-server now can look for public keys
    
    * fixup! fix: serch-server now can look for public keys
    
    * fixup! fix: serch-server now can look for public keys
    
    * fixup! fix: serch-server now can look for public keys
    
    * prettier
    
    * fix: code review

--- apps/search-server/src/services/search-service.ts
+++ apps/search-server/src/services/search-service.ts
@@ -13,8 +13,14 @@ import {
 } from '../types'
 import { ApiError, avnHashRegex, isAxiosError } from '../utils'
 import { elasticSearch } from './elastic-search'
-import { SearchResultBlock, SearchResultExtrinsic } from '@avn/types/src/search'
+import {
+  SearchResultAddress,
+  SearchResultBlock,
+  SearchResultExtrinsic
+} from '@avn/types/src/search'
 import { getMultiSearchQuery } from '../utils/query-builder'
+import { u8aToHex } from '@polkadot/util'
+import { Keyring } from '@polkadot/api'
 
 const logger = getLogger('search-controller')
 
@@ -24,7 +30,16 @@ const defaultSort: EsSortItem = { timestamp: EsSortDirection.Desc }
  * Predefined search fields used for search so far
  */
 const searchFields: Record<string, EsSearchFields> = {
+  address: ['extrinsic.to', 'extrinsic.from', 'proxySigner', 'signer', 'relayer'],
   hash: ['hash']
+}
+
+/** keyring for avn account type */
+const accountKeyring = new Keyring({ type: 'sr25519' })
+
+/** gets hex pk for given ss58 address */
+export function decodeAccountToHex(encodedAddress: string): string {
+  return u8aToHex(accountKeyring.decodeAddress(encodedAddress))
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -40,36 +55,45 @@ const processError = (err: any): Error => {
 
 export const searchForHexAddress = async (value: string) => {
   const isHash = avnHashRegex.test(value)
-
   if (isHash) return await searchForHash(value)
+}
+
+async function performSearch(queryFunction: (value: string[]) => EsQuery, value: string[]) {
+  const size = 1
+  const query = queryFunction(value)
+  const payload: EsRequestPayload = { size, query, sort: [defaultSort] }
+  const path = `${config.db.blocksIndex},${config.db.extrinsicsIndex}/_search`
+
+  const response = await elasticSearch.post<JsonMap>(path, payload)
+  return response.data
 }
 
 export const searchForHash = async (
   value: string
-): Promise<Array<SearchResultBlock | SearchResultExtrinsic>> => {
-  const size = 1
-  const query = getMultiSearchHashQuery([value])
-  const payload: EsRequestPayload = { size, query, sort: [defaultSort] }
-  const path = `${config.db.blocksIndex},${config.db.extrinsicsIndex}/_search`
-
+): Promise<Array<SearchResultBlock | SearchResultExtrinsic | SearchResultAddress>> => {
   try {
-    const { data } = await elasticSearch.post<JsonMap>(path, payload)
-    const items = data.hits.hits.map(hit => {
-      const isBlock = hit._index === config.db.blocksIndex
-      const isExtrinsic = hit._index === config.db.extrinsicsIndex
-
-      if (isBlock) {
-        return {
-          type: 'block' as 'block',
-          ...(hit._source as unknown as SearchBlock)
-        }
-      } else if (isExtrinsic) {
-        return {
-          type: 'extrinsic' as 'extrinsic',
-          ...(hit._source as unknown as SearchExtrinsic)
-        }
-      } else throw new Error('Search item index not found')
-    })
+    let data = await performSearch(getMultiSearchHashQuery, [value])
+    let items = []
+    if (!data.hits.hits.length) {
+      data = await performSearch(getMultiSearchAddressQuery, [value])
+      items = [{ type: 'address' as 'address', publicKey: value }]
+    } else {
+      items = data.hits.hits.map(hit => {
+        const isBlock = hit._index === config.db.blocksIndex
+        const isExtrinsic = hit._index === config.db.extrinsicsIndex
+        if (isBlock) {
+          return {
+            type: 'block' as 'block',
+            ...(hit._source as unknown as SearchBlock)
+          }
+        } else if (isExtrinsic) {
+          return {
+            type: 'extrinsic' as 'extrinsic',
+            ...(hit._source as unknown as SearchExtrinsic)
+          }
+        } else throw new Error('Search item index not found')
+      })
+    }
     return items
   } catch (err) {
     throw processError(err)
@@ -78,3 +102,26 @@ export const searchForHash = async (
 
 const getMultiSearchHashQuery = (values: string[]): EsQuery =>
   getMultiSearchQuery(values, searchFields.hash, 'should')
+
+/**
+ * Gets query for searching for multiple values at the same time
+ * @param {string} values values to search for
+ * @returns {EsQuery} query object for ElasticSearch
+ */
+export const getMultiSearchAddressQuery = (values: string[]): EsQuery => ({
+  bool: {
+    should: values.map(value => getAddressSearchQuery(value))
+  }
+})
+
+/**
+ * Gets query for searching across ES for given address
+ * @param {string} value the address to search for
+ * @returns {EsQuery} query object for ElasticSearch
+ */
+const getAddressSearchQuery = (value: string): EsQuery => ({
+  multi_match: {
+    query: value,
+    fields: searchFields.address
+  }
+})
--- packages/types/src/search/SearchResultItem.ts
+++ packages/types/src/search/SearchResultItem.ts
@@ -4,8 +4,15 @@ import { SearchEvent } from './SearchEvent'
 import { SearchExtrinsic } from './SearchExtrinsic'
 
 interface ISearchResultItem extends EsItem {
-  type: 'block' | 'extrinsic' | 'event'
+  type: 'block' | 'extrinsic' | 'event' | 'address'
+}
+
+interface SearchAddress {
+  publicKey: string
 }
 
 export interface SearchResultBlock extends ISearchResultItem, SearchBlock {}
 export interface SearchResultExtrinsic extends ISearchResultItem, SearchExtrinsic {}
+export interface SearchResultAddress extends SearchAddress {
+  type: 'address'
+}
