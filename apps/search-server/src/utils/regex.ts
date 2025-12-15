ebc9b6d1e00c5a08327f7b1454a14544959d7ce3
todvildes <81619940+todvildes@users.noreply.github.com>
2025-12-15 17:05:42 +0200

    DEVOPS-1217 - update new org name and set arc runner for build workflows (#231)

--- apps/search-server/src/utils/regex.ts
+++ apps/search-server/src/utils/regex.ts
@@ -2,5 +2,5 @@
 //   (lower/upper case, because it matters). Starts with 5 (since the chain is a generic Substrate chain)
 // 3. nftIdRegex: just digits, any length
 export const avnHashRegex = /^0x[A-Fa-f0-9]{64}$/
-// we're not sure about the length (https://github.com/Aventus-Network-Services/block-explorer/pull/19#discussion_r719334128)
+// we're not sure about the length (https://github.com/AventusDAO/block-explorer/pull/19#discussion_r719334128)
 export const nftIdRegex = /^[0-9]*$/
